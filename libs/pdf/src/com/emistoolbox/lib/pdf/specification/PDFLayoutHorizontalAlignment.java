1983450b15d6caffb83783f47e4d7717b1c91ab2
joriki <joriki27@gmail.com>
2016-09-17 14:18:59 +0200

    added alignment with respect to previous component

--- libs/pdf/src/com/emistoolbox/lib/pdf/specification/PDFLayoutHorizontalAlignment.java
+++ libs/pdf/src/com/emistoolbox/lib/pdf/specification/PDFLayoutHorizontalAlignment.java
@@ -3,5 +3,7 @@ package com.emistoolbox.lib.pdf.specification;
 public enum PDFLayoutHorizontalAlignment {
 	LEFT,
 	CENTER,
-	RIGHT
+	RIGHT,
+	BEFORE, // before (to the left of) the previous component
+	AFTER   // after (to the right of) the previous component
 }
--- libs/pdf/src/com/emistoolbox/lib/pdf/specification/PDFLayoutVerticalAlignment.java
+++ libs/pdf/src/com/emistoolbox/lib/pdf/specification/PDFLayoutVerticalAlignment.java
@@ -3,5 +3,7 @@ package com.emistoolbox.lib.pdf.specification;
 public enum PDFLayoutVerticalAlignment {
 	TOP,
 	CENTER,
-	BOTTOM
+	BOTTOM,
+	ABOVE, // above the previous component
+	BELOW  // below the previous component
 }
