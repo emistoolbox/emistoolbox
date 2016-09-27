9efa8809fb8e761150bc7ad053f5b0433144a19e
joriki <joriki27@gmail.com>
2016-09-27 15:33:03 +0200

    deleted obsolete bounding box visitor interface

--- libs/pdf/src/com/emistoolbox/lib/pdf/layout/PDFLayoutBoundingBoxVisitor.java
+++
@@ -1,9 +0,0 @@
-package com.emistoolbox.lib.pdf.layout;
-
-import info.joriki.graphics.Rectangle;
-
-public interface PDFLayoutBoundingBoxVisitor {
-	Rectangle visit (PDFLayoutFrameElement frameElement);
-	Rectangle visit (PDFLayoutFileElement pdfElement);
-	Rectangle visit (PDFLayoutTextElement textElement);
-}
