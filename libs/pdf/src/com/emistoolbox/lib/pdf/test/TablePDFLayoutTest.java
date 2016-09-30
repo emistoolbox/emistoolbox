209790df5dc9a152e97603ecd249ada1e373bbb7
joriki <joriki27@gmail.com>
2016-09-30 10:22:51 +0200

    removed obsolete code

--- libs/pdf/src/com/emistoolbox/lib/pdf/test/TablePDFLayoutTest.java
+++ libs/pdf/src/com/emistoolbox/lib/pdf/test/TablePDFLayoutTest.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import com.emistoolbox.lib.pdf.layout.PDFLayout;
 import com.emistoolbox.lib.pdf.layout.PDFLayoutAlignmentPlacement;
-import com.emistoolbox.lib.pdf.layout.PDFLayoutBorderStyle;
 import com.emistoolbox.lib.pdf.layout.PDFLayoutFont;
 import com.emistoolbox.lib.pdf.layout.PDFLayoutFontStyle;
 import com.emistoolbox.lib.pdf.layout.PDFLayoutFrameElement;
@@ -15,7 +14,6 @@ import com.emistoolbox.lib.pdf.layout.PDFLayoutLineStyle;
 import com.emistoolbox.lib.pdf.layout.PDFLayoutSides;
 import com.emistoolbox.lib.pdf.layout.PDFLayoutTableElement;
 import com.emistoolbox.lib.pdf.layout.PDFLayoutTableFormat;
-import com.emistoolbox.lib.pdf.layout.PDFLayoutTextElement;
 import com.emistoolbox.lib.pdf.layout.PDFLayoutVerticalAlignment;
 
 public class TablePDFLayoutTest {
@@ -23,12 +21,6 @@ public class TablePDFLayoutTest {
 		List<PDFLayout> layouts = new ArrayList<PDFLayout> ();
 		PDFLayout layout = new PDFLayout ();
 		PDFLayoutFrameElement outerFrame = new PDFLayoutFrameElement (500,500);
-//		PDFLayoutFrameElement innerFrame = new PDFLayoutFrameElement (300,300);
-//		innerFrame.position (100,100);
-//		innerFrame.setBorderStyle (new PDFLayoutBorderStyle (new PDFLayoutSides<PDFLayoutLineStyle> (new PDFLayoutLineStyle [] {new PDFLayoutLineStyle (3.,Color.GREEN),new PDFLayoutLineStyle (5.,Color.GREEN),new PDFLayoutLineStyle (8.,Color.GREEN),new PDFLayoutLineStyle (12.,Color.GREEN)}),5.));
-//		innerFrame.setBackgroundColor (new Color (0x7fff0000,true));
-//		innerFrame.addElement (new PDFLayoutTextElement ("Hello World!",new PDFLayoutFont ("Times",14,PDFLayoutFontStyle.BOLD_ITALIC)));
-//		outerFrame.addElement (innerFrame);
 		PDFLayoutTableElement table = new PDFLayoutTableElement ();
 		table.setDimensions (4,3);
 		
