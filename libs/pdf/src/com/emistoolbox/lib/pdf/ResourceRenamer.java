dd206f7634d61afbd7d14ba1b57d7c3e3455ca1e
joriki <joriki27@gmail.com>
2016-10-12 14:25:05 +0200

    can now handle PDF resources in arrays

--- libs/pdf/src/com/emistoolbox/lib/pdf/ResourceRenamer.java
+++ libs/pdf/src/com/emistoolbox/lib/pdf/ResourceRenamer.java
@@ -3,7 +3,9 @@ package com.emistoolbox.lib.pdf;
 import java.io.IOException;
 
 import info.joriki.io.Util;
+import info.joriki.pdf.PDFArray;
 import info.joriki.pdf.PDFDictionary;
+import info.joriki.pdf.PDFObject;
 
 public class ResourceRenamer {
 	String prefix;
@@ -22,13 +24,28 @@ public class ResourceRenamer {
 		String content = new String (Util.toByteArray (page.getContentStream ()),"US-ASCII");
 		PDFDictionary resources = (PDFDictionary) page.get ("Resources");
 		for (String type : resources.keys ()) {
-			PDFDictionary typeResources = (PDFDictionary) resources.get (type);
-			PDFDictionary allTypeResources = allResources.getOrCreateDictionary (type);
-			for (String key : typeResources.keys ()) {
-				String newKey = prefix + ++index;
-				allTypeResources.put (newKey,typeResources.getUnresolved (key));
-				content = content.replaceAll ("/" + key + " ","/" + newKey + " ");
+			PDFObject resourceObject = resources.get (type);
+			if (resourceObject instanceof PDFDictionary) {
+				PDFDictionary typeResources = (PDFDictionary) resourceObject;
+				PDFDictionary allTypeResources = allResources.getOrCreateDictionary (type);
+				for (String key : typeResources.keys ()) {
+					String newKey = prefix + ++index;
+					allTypeResources.put (newKey,typeResources.getUnresolved (key));
+					content = content.replaceAll ("/" + key + " ","/" + newKey + " ");
+				}
 			}
+			else if (resourceObject instanceof PDFArray) {
+				PDFArray typeResources = (PDFArray) resourceObject;
+				PDFArray allTypeResources = (PDFArray) allResources.get (type);
+				if (allTypeResources == null) {
+					allTypeResources = new PDFArray ();
+					allResources.put (type,allTypeResources);
+				}
+				for (PDFObject resource : typeResources)
+					if (!allTypeResources.contains (resource))
+						allTypeResources.add (resource);
+			}
+			else throw new Error ();
 		}
 		return content.getBytes ("US-ASCII");
 	}
